a869b7c4049975f8dca1281d2f4f71da8a5796c7
Torsten Ruger <torsten@villataika.fi>
2012-12-19 12:45:25 +0200

    fix init

--- lib/spree_product_news.rb
+++ lib/spree_product_news.rb
@@ -7,6 +7,12 @@ module SpreeProductNews
         helper ArticlesHelper
       end
 
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*decorator.rb")) do |c|
+        Rails.env.production? ? require(c) : load(c)
+      end
+      Dir.glob(File.join(File.dirname(__FILE__), '../../../app/overrides/*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
       Deface::Override.new(:virtual_path => "spree/admin/orders/show",
                            :name => "converted_admin_order_show_addresses_971422162",
                            :insert_before => "[data-hook='admin_order_show_addresses'], #admin_order_show_addresses[data-hook]",
